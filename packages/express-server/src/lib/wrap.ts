0cce1ddd1a589d45177f9ae5069901e059b974a8
Prabhat Dwivedi <prabhatdwivedi411@icloud.com>
2025-12-22 17:14:41 +0530

    chore: made wrap async

--- packages/express-server/src/lib/wrap.ts
+++ packages/express-server/src/lib/wrap.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 
-export const wrap = (
+export const wrap = async (
 	controller: (req: Request, res: Response) => Promise<void>,
 ) => {
 	return (req: Request, res: Response, next: NextFunction) => {
