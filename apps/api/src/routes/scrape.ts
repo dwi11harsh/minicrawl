c01493d685377172ec57e8bcd44641b552ff181b
Prabhat Dwivedi <prabhatdwivedi411@icloud.com>
2026-02-22 22:29:15 +0530

    chore: scrape request structuring

--- apps/api/src/routes/scrape.ts
+++ apps/api/src/routes/scrape.ts
@@ -1,8 +1,34 @@
 import type { Request, Response } from 'express';
+import { scrapeRequestSchema } from '../zod';
 
-export const scrapeRoute = async (req: Request, res: Response) => {
-	const requestBody = req.body;
+export const scrapeRoute = async (request: Request, response: Response) => {
+	try {
+		// 1. parse incoming request
+		const req = scrapeRequestSchema.safeParse(request.body);
 
-	console.log('/scrape:', requestBody);
-	res.json({ message: 'scrape working' });
+		if (!req.success) {
+			return response.status(400).json({
+				success: false,
+				message: 'Bad Request',
+				error: req.error,
+			});
+		}
+		const data = req.data;
+
+		// 2. create new job id for this request, add metadata and push it to queue
+		console.log(`[NEW SCRAPE REQUEST]: ${data}`);
+
+		// 3. return job id and success true
+		return response.status(201).json({
+			success: true,
+			message: 'a new job request has been created',
+			jobid: '12345',
+		});
+	} catch (e) {
+		return response.status(500).json({
+			success: false,
+			message: 'Unknown error occurred while processing the request',
+			error: e,
+		});
+	}
 };
