c8b3b8521b60474d63e774db9beea0b9e40d34f5
Prabhat Dwivedi <prabhatdwivedi411@icloud.com>
2026-03-09 03:13:24 +0530

    chore: file rename

--- packages/engine/src/browser/initBrowser.ts
+++ packages/engine/src/browser/initBrowser.ts
@@ -1,11 +1,12 @@
 import { logger } from '@repo/logger';
+import fs from 'fs';
 import { chromium, type BrowserContext } from 'patchright';
 import * as path from 'path';
 
 const browserDataPath = path.join(__dirname, '../../../../browser-data');
 
 let context: BrowserContext | null = null;
-let initPromise: Promise<BrowserContext> | null = null;
+let initBrowserPromise: Promise<BrowserContext> | null = null;
 
 export const getBrowserContext = async (
 	headless: boolean = false,
@@ -13,7 +14,7 @@ export const getBrowserContext = async (
 	if (context) return context;
 
 	// launch in progress
-	if (initPromise) return initPromise;
+	if (initBrowserPromise) return initBrowserPromise;
 
 	try {
 		const launchOptions = {
@@ -21,38 +22,56 @@ export const getBrowserContext = async (
 			headless, // non-headless -> harder to detect
 		} as const;
 
-		initPromise = chromium.launchPersistentContext(
+		initBrowserPromise = chromium.launchPersistentContext(
 			browserDataPath, // path to store all cookies
 			{
 				...launchOptions,
 			},
 		);
 
-		context = await initPromise;
-		initPromise = null;
+		context = await initBrowserPromise;
+		initBrowserPromise = null;
 
 		context.on('close', () => {
 			logger.warn('⚠️ browser context closed unexpectedly');
 			context = null;
-			initPromise = null;
+			initBrowserPromise = null;
 		});
 
 		return context;
 	} catch (e) {
-		initPromise = null;
+		initBrowserPromise = null;
 		logger.error('❗️error getting browser', (e as any).message);
 
+		// single retry
 		try {
-			// single retry
-			initPromise = chromium.launchPersistentContext(browserDataPath, {
-				channel: 'chrome',
-				headless,
+			fs.rmSync(browserDataPath, {
+				recursive: true,
+				force: true,
 			});
-			context = await initPromise;
-			initPromise = null;
+			logger.info('browser data has been cleared...');
+
+			// with 'chromium' channel as 'chrome' might be the cause of the error
+			initBrowserPromise = chromium.launchPersistentContext(
+				browserDataPath,
+				{
+					channel: 'chromium',
+					headless,
+				},
+			);
+
+			context = await initBrowserPromise;
+
+			initBrowserPromise = null;
+
+			context.on('close', () => {
+				logger.warn('⚠️ browser context closed unexpectedly');
+				context = null;
+				initBrowserPromise = null;
+			});
 			return context;
 		} catch (retryError) {
-			initPromise = null;
+			initBrowserPromise = null;
 			logger.error(
 				'❗️ browser launch failed after retry',
 				(retryError as any).message,
